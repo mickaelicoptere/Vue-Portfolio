7b21df66b432d3eeec67cdf40fae483a745e02e3
Mickael Lanier <mickael.lanier@etu.unice.fr>
2018-05-01 17:50:07 +0200

    +nav drawer

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,57 +3,70 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Mickaelicoptere',
+    title: "Mickaelicoptere",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'portfolio using vueJS, vuetify, bulma css and NuxtJS' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "portfolio using vueJS, vuetify, bulma css and NuxtJS"
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.0/css/bulma.min.css' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Source+Code+Pro:400,700' },
-      { rel: 'stylesheet', href:'https://unpkg.com/vuetify/dist/vuetify.min.css'},
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'}
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.0/css/bulma.min.css"
+      },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css?family=Source+Code+Pro:400,700"
+      },
+      {
+        rel: "stylesheet",
+        href: "https://unpkg.com/vuetify/dist/vuetify.min.css"
+      },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons"
+      }
     ]
   },
 
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: "#3B8070" },
 
-    plugins: [
-      { src: '~/plugins/vuetify.js', ssr: false },
-      { src: '~plugins/ga.js', ssr: false }
-    ],
+  plugins: [
+    { src: "~/plugins/vuetify.js", ssr: false },
+    { src: "~plugins/ga.js", ssr: false }
+  ],
 
-    css: [
-      'assets/css/main.css'
-    ],
-  
+  css: ["assets/css/main.css", "assets/css/animate.css"],
+
   /*
   ** Build configuration
   */
   build: {
-    vendor: [
-      '~/plugins/vuetify.js',
-    ],
+    vendor: ["~/plugins/vuetify.js"],
     extractCSS: true,
 
-  
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
       }
     }
   }
-}
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="section is-large">
     <div class="container has-text-centered">
+      <!-- <transition v-on:enter="enter" -->
         <h1 id="title">Hi, I'm Mickael Lanier</h1>
       <h2 class="subtitle">I'm a <p id="bold">{{whoami}}</p> {{whatido}}</h2>
     </div>
@@ -8,17 +9,19 @@
 </template>
 
 <script>
-export default{
+export default {
   data: () => ({
     whoami: "co-op student",
-    whatido: "at UNSA institute of technology and i work for the Rector of Nice academy as an IT tech."
+    whatido:
+      "at UNSA institute of technology and i work for the Rector of Nice academy as an IT tech."
   })
-}
+};
 </script>
 
 <style>
 #title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   color: #35495e;
   letter-spacing: 1px;
   align-items: center;
