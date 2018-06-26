89e4ed6c8ca83cca38843fefbc39ee1d1aef0e91
Mickael Lanier <mickael.lanier@etu.unice.fr>
2018-06-26 21:58:03 +0200

    +LandigPageProjects & stuff

--- components/LandingPageProjects.vue
+++ components/LandingPageProjects.vue
@@ -4,50 +4,18 @@
             <v-flex xs12>
                 <v-container grid-list-xl>
                 <v-layout row wrap align-center>
-                    <v-flex xs12 md4>
-                    <v-card class="elevation-0 transparent">
-                        <v-card-text class="text-xs-center">
-                        <v-icon x-large class="blue--text text--lighten-2">color_lens</v-icon>
-                        </v-card-text>
-                        <v-card-title primary-title class="layout justify-center">
-                        <div class="headline text-xs-center">Material Design</div>
-                        </v-card-title>
-                        <v-card-text>
-                        Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. 
-                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
-                        Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti. 
-                        </v-card-text>
-                    </v-card>
-                    </v-flex>
-                    <v-flex xs12 md4>
-                    <v-card class="elevation-0 transparent">
-                        <v-card-text class="text-xs-center">
-                        <v-icon x-large class="blue--text text--lighten-2">flash_on</v-icon>
-                        </v-card-text>
-                        <v-card-title primary-title class="layout justify-center">
-                        <div class="headline">Fast development</div>
-                        </v-card-title>
-                        <v-card-text>
-                        Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. 
-                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
-                        Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti. 
-                        </v-card-text>
-                    </v-card>
-                    </v-flex>
-                    <v-flex xs12 md4>
-                    <v-card class="elevation-0 transparent">
-                        <v-card-text class="text-xs-center">
-                        <v-icon x-large class="blue--text text--lighten-2">build</v-icon>
-                        </v-card-text>
-                        <v-card-title primary-title class="layout justify-center">
-                        <div class="headline text-xs-center">Completely Open Sourced</div>
-                        </v-card-title>
-                        <v-card-text>
-                        Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. 
-                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
-                        Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti. 
-                        </v-card-text>
-                    </v-card>
+                    <v-flex xs12 md4 v-for="p in projects" :key="p">
+                        <v-card class="elevation-0 transparent">
+                            <v-card-text class="text-xs-center">
+                                <v-icon x-large class="blue--text text--lighten-2">{{p.icon}}</v-icon>
+                            </v-card-text>
+                            <v-card-title primary-title class="layout justify-center">
+                                <div class="headline text-xs-center">{{p.title}}</div>
+                            </v-card-title>
+                            <v-card-text>
+                                {{p.text}}
+                            </v-card-text>
+                        </v-card>
                     </v-flex>
                 </v-layout>
             </v-container>
@@ -57,6 +25,27 @@
 
 <script>
 export default {
+    data: () => ({
+        projects: [
+            {
+                icon: "color_lens",
+                title: "Material Design Askip",
+                text: "Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti."
+            },
+            {
+                icon: "flash_on",
+                title: "Fast dev",
+                text: "Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti."
+            },
+            {
+                icon: "build",
+                title: "Fast dev",
+                text: "Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti."
+            }
+
+        ]
+
+    })
     
 }
 </script>
